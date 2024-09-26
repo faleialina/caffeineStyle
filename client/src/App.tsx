6a15ee82d5d57785629a365542530f19e842a073
faleialina <august3@mail.ru>
2024-09-26 20:19:37 +0300

    adding sections

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,73 @@
 import styled, { createGlobalStyle } from 'styled-components'
 import coffee from './assets/CoffeeGrains.png'
+import badge from './assets/badge.png'
+import best_price from './assets/bestprice.png'
 import img from './assets/coffeeImage.png'
+import coffeeMenu1 from './assets/coffeeMenu1.svg'
+import coffeeMenu2 from './assets/coffeeMenu2.svg'
+import coffeeMenu3 from './assets/coffeeMenu3.svg'
+import coffeeMenu4 from './assets/coffeeMenu4.svg'
+import coffee_beans from './assets/coffeebeans.png'
+import coffee_cup from './assets/coffeecup.png'
 import Header from './components/Header'
 
-// const coffies = [
-//   { id: 1, title: 'Cappuccino', description: 'Coffee 50% | Milk 50%', price: '$8.50', img: coffeeMenu1 },
-//   { id: 2, title: 'Chai Latte', description: 'Coffee 50% | Milk 50%', price: '$8.50', img: coffeeMenu2 },
-//   { id: 3, title: 'Macchiato', description: 'Coffee 50% | Milk 50%', price: '$8.50', img: coffeeMenu3 },
-//   { id: 4, title: 'Expresso', description: 'Coffee 50% | Milk 50%', price: '$8.50', img: coffeeMenu4 },
-// ];
+const coffies = [
+	{
+		id: 1,
+		title: 'Cappuccino',
+		description: 'Coffee 50% | Milk 50%',
+		price: '$8.50',
+		img: coffeeMenu4,
+	},
+	{
+		id: 2,
+		title: 'Chai Latte',
+		description: 'Coffee 50% | Milk 50%',
+		price: '$8.50',
+		img: coffeeMenu3,
+	},
+	{
+		id: 3,
+		title: 'Macchiato',
+		description: 'Coffee 50% | Milk 50%',
+		price: '$8.50',
+		img: coffeeMenu2,
+	},
+	{
+		id: 4,
+		title: 'Expresso',
+		description: 'Coffee 50% | Milk 50%',
+		price: '$8.50',
+		img: coffeeMenu1,
+	},
+]
+
+const advantages = [
+	{
+		id: 1,
+		title: 'Supreme Beans',
+		description: 'Beans that provides great taste',
+		icon: coffee_beans,
+	},
+	{
+		id: 2,
+		title: 'High Quality',
+		description: 'We provide the highest quality',
+		icon: badge,
+	},
+	{
+		id: 3,
+		title: 'Extraordinary',
+		description: 'Coffee like you have never tasted',
+		icon: coffee_cup,
+	},
+	{
+		id: 4,
+		title: 'Affordable Price',
+		description: 'Our Coffee prices are easy to afford',
+		icon: best_price,
+	},
+]
 
 const Global = createGlobalStyle`
   * {
@@ -87,6 +146,150 @@ const CoffeeDiscovery = styled.div`
 		}
 	}
 `
+const Menu = styled.div`
+	max-width: 1220px;
+	margin: auto;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	gap: 20px;
+
+	h2 {
+		font-family: 'Playfair Display';
+		font-size: 54px;
+		font-weight: 700;
+		color: #603809;
+	}
+
+	p {
+		font-family: 'Playfair Display';
+		font-size: 20px;
+		font-weight: 400;
+		color: #603809;
+	}
+
+	.content {
+		display: flex;
+		justify-content: center;
+		align-items: flex-start;
+		flex-direction: row;
+		gap: 30px;
+		flex-wrap: wrap;
+
+		.item {
+			background-color: #fff9f1;
+			border: 1px solid #f9c06a;
+			max-width: 280px;
+			max-height: 364px;
+
+			.itemInfo {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				gap: 8px;
+				padding: 14px 0 42px;
+
+				h3 {
+					font-family: 'Playfair Display';
+					font-size: 22px;
+					font-weight: 600;
+					color: #603809;
+				}
+
+				p {
+					font-family: 'Playfair Display';
+					font-size: 16px;
+					font-weight: 400;
+					color: #1e1e1e;
+				}
+
+				.price {
+					font-family: 'Playfair Display';
+					font-size: 18px;
+					font-weight: 700;
+					color: #603809;
+				}
+			}
+		}
+	}
+`
+
+const Button = styled.button`
+	display: block;
+	border: none;
+	background: #f9c06a;
+	padding: 14px 27px;
+	border-radius: 24px;
+	color: #1e1e1e;
+	max-width: 155px;
+	font-family: 'Playfair Display';
+	font-size: 16px;
+	font-weight: 700;
+	cursor: pointer;
+`
+
+const Advantages = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	margin: 82px auto;
+	gap: 20px;
+
+	h2 {
+		font-family: Playfair Display;
+		font-size: 54px;
+		font-weight: 700;
+		color: #603809;
+	}
+
+	p {
+		font-family: Playfair Display;
+		font-size: 20px;
+		font-weight: 400;
+		color: #707070;
+	}
+
+	.advantages_content {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 20px;
+
+		.advantages_card {
+			border: 1px solid #f9c06a6b;
+			max-width: 280px;
+			height: 284px;
+			text-align: center;
+			background: #fff9f1;
+			padding: 36px 43px;
+
+			.coffee_beans,
+			.badge,
+			.coffee_cup,
+			.best_price {
+				width: 88px;
+				height: 88px;
+				background-size: contain;
+				background-repeat: no-repeat;
+			}
+
+			h3 {
+				font-family: Playfair Display;
+				font-size: 22px;
+				font-weight: 700;
+				color: #603809;
+				margin: 25px auto 8px;
+			}
+		}
+	}
+	.important {
+		font-family: Playfair Display;
+		font-size: 20px;
+		font-weight: 700;
+		color: #603809;
+	}
+`
 
 const App = () => {
 	return (
@@ -121,18 +324,44 @@ const App = () => {
 					<img src={coffee} alt='coffee' />
 				</div>
 			</CoffeeDiscovery>
-			<div className='menu'>
+			<Menu>
 				<h2>Enjoy a new blend of coffee style</h2>
 				<p>
 					Explore all flavours of coffee with us. There is always a new cup
 					worth experiencing.
 				</p>
 				<div className='content'>
-					{/* {coffies.map(el => (
-            
-          ))} */}
+					{coffies.map((el, index) => (
+						<div key={index} className='item'>
+							<div>
+								<img src={el.img} alt='coffee' />
+							</div>
+							<div className='itemInfo'>
+								<h3>{el.title}</h3>
+								<p>{el.description}</p>
+								<p className='price'>{el.price}</p>
+								<Button>Order Now</Button>
+							</div>
+						</div>
+					))}
 				</div>
-			</div>
+			</Menu>
+			<Advantages>
+				<h2>Why are we different?</h2>
+				<p>We don’t just make your coffee, we make your day!</p>
+				<div className='advantages_content'>
+					{advantages.map(el => (
+						<div className='advantages_card' key={el.id}>
+							<img src={el.icon} alt='' />
+							<h3>{el.title}</h3>
+							<p>{el.description}</p>
+						</div>
+					))}
+				</div>
+				<p>Great ideas start with great coffee, Lets help you achieve that</p>
+				<p className='important'>Get started today.</p>
+				<Button>Join Us</Button>
+			</Advantages>
 		</>
 	)
 }
